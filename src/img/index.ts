55c723a4a9a946d7596093f1c2d9ec731cc4a74a
Travis Craft <tlcraft@users.noreply.github.com>
2021-08-21 13:45:04 -0600

    Add images and index file.

---
+++ src/img/index.ts
@@ -0,0 +1,12 @@
+import crate from './crate.jpg';
+import door from './door/Door_Wood_001_basecolor.jpg';
+import doorAmbientOcclusion from './door/Door_Wood_001_ambientOcclusion.jpg';
+import doorHeight from './door/Door_Wood_001_height.png';
+import doorMetallic from './door/Door_Wood_001_metallic.jpg';
+import doorNormal from './door/Door_Wood_001_normal.jpg';
+import doorOpacity from './door/Door_Wood_001_opacity.jpg';
+import doorRoughness from './door/Door_Wood_001_roughness.jpg';
+import gradient from './gradient.png';
+import ice from './ice.png';
+
+export { crate, door, doorAmbientOcclusion, doorHeight, doorMetallic, doorNormal, doorOpacity, doorRoughness, gradient, ice };
