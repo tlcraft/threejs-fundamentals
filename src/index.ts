f29acc95a146cb0b1ecf49e632945dd471f2e76c
Travis Craft <tlcraft@users.noreply.github.com>
2022-02-08 17:27:45 -0700

    Change particle color.

--- src/index.ts
+++ src/index.ts
@@ -853,7 +853,7 @@ function generateRandomParticles(): Points {
 
     const geometry = new BufferGeometry();
     geometry.setAttribute('position', positionsAttribute);
-    const material = new PointsMaterial( {size: 0.1, sizeAttenuation: true} );
+    const material = new PointsMaterial( {size: 0.1, sizeAttenuation: true, color: new Color('#ff3300') } );
     const mesh = new Points( geometry, material );
     mesh.position.set(7, 0, 12);
     return mesh;
